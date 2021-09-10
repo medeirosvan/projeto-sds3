278f76d72b9d8582dbd8213894c17bda2689d4b7
Vanessa Medeiros <medeiros.van@gmail.com>
2021-09-10 18:10:12 -0300

    Correção

--- frontend/src/pages/Dashboard/index.tsx
+++ frontend/src/pages/Dashboard/index.tsx
@@ -9,7 +9,7 @@ const Dashboard = () => {
         <>
             <NavBar />
             <div className="container">
-                <h1 className="text-primary py-3">Dashboards de Vendas</h1>
+                <h1 className="text-primary py-3">Dashboard de Vendas</h1>
 
                 <div className="row px-3" >
                     <div className="col-sm-6">
